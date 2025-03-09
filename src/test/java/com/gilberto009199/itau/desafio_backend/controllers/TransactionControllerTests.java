74c2d291718b46b94410d79fae34f46222427b13
gilberto-009199 <gilberto.tec@vivaldi.net>
2025-03-09 12:28:51 -0300

    bug: tests background test spring not configured

--- src/test/java/com/gilberto009199/itau/desafio_backend/controllers/TransactionControllerTests.java
+++ src/test/java/com/gilberto009199/itau/desafio_backend/controllers/TransactionControllerTests.java
@@ -6,14 +6,17 @@ import java.time.OffsetDateTime;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.gilberto009199.itau.desafio_backend.requests.TransactionRequest;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ExtendWith(SpringExtension.class)
 public class TransactionControllerTests {
  
     /* Componente para interagir com a API */
@@ -22,11 +25,14 @@ public class TransactionControllerTests {
 
     @Test
     void createTransactionValid(){
+        
+        String path = "/transacao";
+
         var request = new TransactionRequest(
             BigDecimal.valueOf(100),
             OffsetDateTime.now()
         );
-		String path = "/transaction";
+		
 
         var response = restAPI.postForEntity( path, request, null);
 
