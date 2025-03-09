c1b9f6ba4adc08a95ef960af24e6bf739140c0f1
gilberto-009199 <gilberto.tec@vivaldi.net>
2025-03-09 12:33:33 -0300

    tests: valid transaction ok, invalid Transaction error

--- src/test/java/com/gilberto009199/itau/desafio_backend/controllers/TransactionControllerTests.java
+++ src/test/java/com/gilberto009199/itau/desafio_backend/controllers/TransactionControllerTests.java
@@ -3,13 +3,17 @@ package com.gilberto009199.itau.desafio_backend.controllers;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.OffsetDateTime;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalUnit;
 
+import org.assertj.core.data.TemporalUnitOffset;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.context.annotation.Description;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -24,6 +28,7 @@ public class TransactionControllerTests {
 	protected TestRestTemplate restAPI;
 
     @Test
+    @Description("Tests Transaction valid value and date")
     void createTransactionValid(){
         
         String path = "/transacao";
@@ -39,7 +44,24 @@ public class TransactionControllerTests {
 
         Assertions.assertEquals( HttpStatus.CREATED, response.getStatusCode());
 
-
     }
 
+    @Test
+    @Description("Tests Transaction with Time after now")
+    void createTransactionInValidDate(){
+        
+        String path = "/transacao";
+
+        var request = new TransactionRequest(
+            BigDecimal.valueOf(100),
+            OffsetDateTime.now().plus(100, ChronoUnit.MINUTES)
+        );
+		
+
+        var response = restAPI.postForEntity( path, request, null);
+
+
+        Assertions.assertEquals( HttpStatus.UNPROCESSABLE_ENTITY, response.getStatusCode());
+
+    }
 }
