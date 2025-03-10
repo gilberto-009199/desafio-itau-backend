0daafc577d61b37ceac11041cf1c4b6420fbf591
gilberto-009199 <gilberto.tec@vivaldi.net>
2025-03-09 22:03:18 -0300

    remove comment

--- src/main/java/com/gilberto009199/itau/desafio_backend/validation/BeforeNowTimeValid.java
+++ src/main/java/com/gilberto009199/itau/desafio_backend/validation/BeforeNowTimeValid.java
@@ -6,7 +6,7 @@ import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
 public class BeforeNowTimeValid implements ConstraintValidator<BeforeNowTime, OffsetDateTime> {
-    // Valida Transaction datetime
+
     public boolean isValid(OffsetDateTime date, ConstraintValidatorContext cxt) {
         return date.isBefore(OffsetDateTime.now());
     }
